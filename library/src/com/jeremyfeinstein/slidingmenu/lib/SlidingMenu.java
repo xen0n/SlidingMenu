526934b13de0ecb800b31ad7de145a96cacc23e7
Steve Lhomme <slhomme@levelupstudio.com>
2013-04-11 10:16:36 +0200

    Help cleaning the memory by removing some references after the widgets is detached from its window

--- library/src/com/jeremyfeinstein/slidingmenu/lib/SlidingMenu.java
+++ library/src/com/jeremyfeinstein/slidingmenu/lib/SlidingMenu.java
@@ -282,6 +282,22 @@ public class SlidingMenu extends FrameLayout {
 		ta.recycle();
 	}
 
+	@Override
+	protected void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+		if (null != mViewAbove) {
+			mViewAbove.setCustomViewBehind(null);
+			mViewAbove.setOnPageChangeListener(null);
+			mViewAbove.removeAllViews();
+			mViewAbove = null;
+		}
+		if (null != mViewBehind) {
+			mViewBehind.setCustomViewAbove(null);
+			mViewBehind.removeAllViews();
+			mViewBehind = null;
+		}
+	}
+
 	/**
 	 * Attaches the SlidingMenu to an entire Activity
 	 * 
@@ -1010,8 +1026,12 @@ public class SlidingMenu extends FrameLayout {
 			mHandler.post(new Runnable() {
 				public void run() {
 					Log.v(TAG, "changing layerType. hardware? " + (layerType == View.LAYER_TYPE_HARDWARE));
-					getContent().setLayerType(layerType, null);
-					getMenu().setLayerType(layerType, null);
+					if (mViewAbove != null && getContent() != null) {
+						getContent().setLayerType(layerType, null);
+					}
+					if (mViewBehind != null && getMenu() != null) {
+						getMenu().setLayerType(layerType, null);
+					}
 					if (getSecondaryMenu() != null) {
 						getSecondaryMenu().setLayerType(layerType, null);
 					}
