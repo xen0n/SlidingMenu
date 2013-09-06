8bff013461f45747f688db23fed21989d11043be
Wang Xuerui <idontknw.wang@gmail.com>
2013-09-06 16:16:35 +0800

    reduce method visibility for consistency with common practice

--- library/src/com/jeremyfeinstein/slidingmenu/lib/app/RoboSherlockSlidingPreferenceActivity.java
+++ library/src/com/jeremyfeinstein/slidingmenu/lib/app/RoboSherlockSlidingPreferenceActivity.java
@@ -19,7 +19,7 @@ public class RoboSherlockSlidingPreferenceActivity extends
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		mHelper = new SlidingActivityHelper(this);
 		super.onCreate(savedInstanceState);
 		mHelper.onCreate(savedInstanceState);
@@ -31,7 +31,7 @@ public class RoboSherlockSlidingPreferenceActivity extends
 	 * @see android.app.Activity#onPostCreate(android.os.Bundle)
 	 */
 	@Override
-	public void onPostCreate(Bundle savedInstanceState) {
+	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
 		mHelper.onPostCreate(savedInstanceState);
 	}
